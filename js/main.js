86d83b2a4057f3b5fdf02dc808a62030d7aae2df
SIJED <Danilcarol56@gmail.com>
2024-02-02 21:46:13 +0700

    install

---
+++ js/main.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            column1: [],
+            column2: [],
+            column3: [],
+            newCardTitle: '',
+            newItemText: '',
+            check: true,
+
+        }
+    },
+    methods: {
+
+    },
+})

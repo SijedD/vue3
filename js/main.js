440d4c5e042717b19693da401e805c6a76bb53fd
SIJED <Danilcarol56@gmail.com>
2024-02-02 22:19:58 +0700

    Add card

--- js/main.js
+++ js/main.js
@@ -5,13 +5,35 @@ new Vue({
             column1: [],
             column2: [],
             column3: [],
+            column4: [],
             newCardTitle: '',
             newItemText: '',
-            check: true,
+            newCardDesc: '',
+            deadline: '',
 
         }
     },
     methods: {
+        addCard() {
+            const newCard = {
+                id: Date.now(),
+                title: this.newCardTitle,
+                description: this.newCardDesc,
+                deadline: this.deadline,}
+            this.column1.push(newCard);
+            this.newCardDesc = '';
+            this.newCardTitle = '';
+            this.newItemText = '';
+            this.deadline = '';
+            newCard.completedDate = new Date().toLocaleString(); // добавляем дату и время завершения
+            }
 
-    },
+        }
+
+
+
+
+
+
 })
+
